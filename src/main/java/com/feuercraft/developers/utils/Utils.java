5d0c3817282083b45847ef74ef7151f113d6237b
Andreas Butzmuehlen <andreas.butzmuehlen@outlook.de>
2020-05-03 20:58:50 +0200

    Config Files modiefied

--- src/main/java/com/feuercraft/developers/utils/Utils.java
+++ src/main/java/com/feuercraft/developers/utils/Utils.java
@@ -1,14 +1,13 @@
 package com.feuercraft.developers.utils;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 
 public class Utils {
 
     public static String prefix;
 
     public static void sendConsole(String msg){
-        Bukkit.getConsoleSender().sendMessage(msg.replace("&","$"));
+        Bukkit.getConsoleSender().sendMessage(prefix + msg);
     }
 
 
