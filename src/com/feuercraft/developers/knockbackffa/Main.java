2863bdb3dc0dc45485d96a5ddc7e163bb00a376a
Andreas Butzmuehlen <andreas.butzmuehlen@outlook.de>
2020-05-03 14:51:13 +0200

    initial Commit

---
+++ src/com/feuercraft/developers/knockbackffa/Main.java
@@ -0,0 +1,17 @@
+package com.feuercraft.developers.knockbackffa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //TODO Plugin start-logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        //TODO Plugin stop-logic
+    }
+}
